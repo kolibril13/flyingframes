78cfb9afbd6ae430fa2ee52212a045b4f5b8ce79
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2021-09-18 11:47:03 +0200

    renamed _static to imgs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -95,4 +95,4 @@ if not os.path.exists('media/videos/480p30'):
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static', 'media/images', 'media/videos/480p30']
+html_static_path = ['_static', 'imgs']
